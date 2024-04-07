d887a8298853e0ff79e3f010f6956f39690ff306
Viraj Mody <ac117139@avcol.school.nz>
2024-04-08 00:07:12 +1200

    updated basically everything

--- SapphireSports/Controllers/HomeController.cs
+++ SapphireSports/Controllers/HomeController.cs
@@ -63,6 +63,11 @@ namespace SapphireSports.Controllers
             return View();
         }
 
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
