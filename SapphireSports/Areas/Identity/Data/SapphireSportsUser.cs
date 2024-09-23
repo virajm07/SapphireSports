b15222b14c884b0c51a5fc186df66883ad85fd40
Viraj Mody <ac117139@avcol.school.nz>
2024-09-24 11:10:26 +1200

    spelling errors

--- SapphireSports/Areas/Identity/Data/SapphireSportsUser.cs
+++ SapphireSports/Areas/Identity/Data/SapphireSportsUser.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace SapphireSports.Areas.Identity.Data
 {
     public class SapphireSportsUser : IdentityUser
     {
+
         public string FirstName {  get; set; }
         public string LastName { get; set; }
     }
