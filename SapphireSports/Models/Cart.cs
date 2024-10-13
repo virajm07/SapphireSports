c372d033132ca7254d0d773e15a542d35a40f30a
Viraj Mody <ac117139@avcol.school.nz>
2024-10-13 23:53:06 +1300

    small updates

--- SapphireSports/Models/Cart.cs
+++ SapphireSports/Models/Cart.cs
@@ -11,19 +11,23 @@ namespace SapphireSports.Models
 
         [Required]
         [DisplayName("Product Name")]
+        //Field will display as Product Name
         public int ProductID { get; set; }
 
         [Required]
         [DisplayName("Order Date")]
+        //Field will display as Order Date
         public int OrderID { get; set; }
 
         [Required]
         [DisplayName("Price")]
+        //Field will display as Price
         public int ListPrice { get; set; }
 
         [Required]
         public int Quantity { get; set; }
         public Order Order { get; set; }
+        //One to one relationship with the Orders table
         public Product Product { get; set; }
         //One to one relationship with the Products table
     }
--- SapphireSports/Models/Categories.cs
+++ SapphireSports/Models/Categories.cs
@@ -8,16 +8,22 @@ namespace SapphireSports.Models
         [Key] public int CategoriesID { get; set; }
         //Indicates that CategoriesID is the primary key in this table
         public required string Cricket { get; set; }
+        //String allows the user to type any characters in the field upto 256 words
 
         public required string Football { get; set; }
+        //String allows the user to type any characters in the field upto 256 words
 
         public required string Basketball { get; set; }
+        //String allows the user to type any characters in the field upto 256 words
 
         public required string Rugby { get; set; }
+        //String allows the user to type any characters in the field upto 256 words
 
         public required string Tennis { get; set; }
+        //String allows the user to type any characters in the field upto 256 words
 
         public required string Shoes { get; set; }
+        //String allows the user to type any characters in the field upto 256 words
 
         public ICollection<Product> Products { get; set; } = new List<Product>();
         //One to many relationship with the Products table
